08c8e92267d6113d604c780c38159de589961990
Jason Cheatham <j.cheatham@gmail.com>
2016-12-14 00:58:00 -0500

    Use intern-dev for building

--- support/lint.ts
+++
@@ -1,9 +0,0 @@
-import { echo } from 'shelljs';
-import { exec } from './common';
-
-echo('### Linting');
-echo('### Linting code');
-exec('./node_modules/.bin/tslint --project tsconfig.json');
-echo('### Linting tests');
-exec('./node_modules/.bin/tslint --project tests/tsconfig.json');
-echo('### Done linting');
